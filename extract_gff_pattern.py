30a099b87a04a1a37f51bc552455c49c5ffe6f51
Torben Nielsen <mjoelnir@me.com>
2018-08-27 15:57:43 -0700

    Updated a few things in extract_gff_pattern.py.

--- extract_gff_pattern.py
+++ extract_gff_pattern.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python
 
 import argparse
-import os
 import subprocess
 import io
+import os
 
-try:
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("directory", type = str, help = "directory where the files are")
-    arg_parser.add_argument("pattern", type = str, help = "pattern to look for")
-    arg_parser.add_argument("score", type = int, help = "score threshold")
-    arguments = arg_parser.parse_args()
+def extract_gff_pattern(directory, pattern, score):
 
-    directory = os.path.realpath(arguments.directory)
-    pattern = arguments.pattern
-    score = arguments.score
+    directory = os.path.realpath(directory)
 
     print(directory, pattern, score)
 
@@ -24,20 +17,30 @@ try:
 
     response = subprocess.run(f"grep {pattern} " + gff_filename + "|" +
                               f"awk '{{if($6 >= {score}) print}}'",
-                              stdout = subprocess.PIPE,
-                              shell = True, env = dict(os.environ), encoding='utf-8')
+                              stdout=subprocess.PIPE,
+                              shell=True, env=dict(os.environ), encoding='utf-8')
     output = io.StringIO(response.stdout)
 
     for line in output:
         line = line.split()
         if line[6] == '+':
             subprocess.run(f'samtools faidx {fasta_filename} \
-                            {line[0]}:{line[3]}-{line[4]} | seqtk seq -l 0',
-                            shell = True, env = dict(os.environ), encoding='utf-8')
+                           {line[0]}:{line[3]}-{line[4]} | seqtk seq -l 0',
+                           shell=True, env=dict(os.environ), encoding='utf-8')
         elif line[6] == '-':
             subprocess.run(f'samtools faidx {fasta_filename} \
-                            {line[0]}:{line[3]}-{line[4]} | seqtk seq -r',
-                            shell = True, env = dict(os.environ), encoding='utf-8')
+                           {line[0]}:{line[3]}-{line[4]} | seqtk seq -r',
+                           shell=True, env=dict(os.environ), encoding='utf-8')
 
-except KeyboardInterrupt:
-    pass
+if __name__ == '__main__':
+    try:
+        arg_parser = argparse.ArgumentParser()
+        arg_parser.add_argument("directory", type = str, help = "directory where the files are")
+        arg_parser.add_argument("pattern", type = str, help = "pattern to look for")
+        arg_parser.add_argument("score", type = int, help = "score threshold")
+        arguments = arg_parser.parse_args()
+
+        extract_gff_pattern(arguments.directory, arguments.pattern, arguments.score)
+
+    except KeyboardInterrupt:
+        pass
